301f9439b9374a11b97c7f2f0d788683513b2a16
MathiasFar <mathiassvarre06@gmail.com>
2023-01-31 14:25:44 +0100

    Implemented char freq and fixed priority queue

--- main.cpp
+++ main.cpp
@@ -3,51 +3,29 @@
 #include <string>
 #include <sstream>
 #include <vector>
-/* #include "pQueue.h" */
-#include "tree.h"
 
 // References:
 //     https://resources.nerdfirst.net/huffman
 
 // TODO:
 //     [+] Priority Queue
-//     [] Word Frequency
+//     [+] Word Frequency
 //     [] Huffman Algorithm
 
-
-
-
-std::vector<std::string> splitStr(std::string &s, char del) {
-    std::vector<std::string> tokens;
-    std::stringstream ss(s);
-    std::string token;
-    while(getline(ss, token, del)) {
-        tokens.push_back(token);
-    }
-    return tokens;
-}
-
-
 std::vector<Node*> termFreq(std::string data) {
-    std::vector<std::string> words = splitStr(data, ' ');
     std::vector<Node*> terms;
-    for(auto &word : words) {
-        if(terms.empty()) {
-            Node *new_node = new Node();
-            new_node->word = word;
-            new_node->freq = 1;
-            terms.push_back(new_node);
-        } else {
-            for(int j = 0; j < terms.size(); ++j) {
-                if(std::strcmp(terms[j]->word.c_str(), word.c_str()) == 0) {
-                    terms[j]->freq += 1; 
-                } else {
-                    Node *new_node = new Node();
-                    new_node->word = word;
-                    new_node->freq = 1;
-                    terms.push_back(new_node);
-                }
+    for(auto &ch : data) {
+        bool found = false;
+        for(auto &node : terms) {
+            if(node->chr == ch) {
+                ++node->freq;
+                found = true;
+                break;
             }
+        }
+        if(!found) {
+            Node *new_node = new Node{1, ch};
+            terms.push_back(new_node);
         }
     }
     return terms;
@@ -56,10 +34,22 @@ std::vector<Node*> termFreq(std::string data) {
 int main() {
 
     std::string data = "hello world hello";
-    std::vector<Node*> q = termFreq(data);
+    std::vector<Node*> cf = termFreq(data);
+    std::vector<Node*> pQueue;
+    for(auto &leaf : cf) {
+        int i = 0;
+        if(pQueue.size() > 0) {
+            while(i < pQueue.size() and leaf->freq < pQueue[i]->freq) {
+                ++i;
+            }
+            pQueue.insert(pQueue.begin()+i, leaf);
+        } else {
+            pQueue.push_back(leaf);
+        }
+    }
 
-    for(auto &n : q) {
-        std::cout<<"Word: "<<n->word<<std::endl;
+    for(auto &n : pQueue) {
+        std::cout<<"Char: "<<"'"<<n->chr<<"'"<<std::endl;
         std::cout<<"Freq: "<<n->freq<<std::endl;
         std::cout<<std::endl;
     }
